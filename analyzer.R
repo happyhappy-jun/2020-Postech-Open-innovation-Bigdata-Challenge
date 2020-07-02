f5f9d0f65fd2d366ba7d93c413e8a7a2c8121fce
YangSeungWon <35984562+YangSeungWon@users.noreply.github.com>
2020-07-03 01:48:13 +0900

    use POSIXct type

--- analyzer.R
+++ analyzer.R
@@ -1,24 +1,25 @@
 setwd("~/Github/2020-Postech-Open-innovation-Bigdata-Challenge")
 
-fac2chr <- function(data){
-  i <- sapply(data, is.factor)
-  data[i] <- lapply(data[i], as.character)
+
+# import daily weather data
+importWeather <- function(){
+  data_t_n<-read.csv(file="daily_temperatures_national.csv",stringsAsFactors=FALSE)
+  data_t_p<-read.csv(file="daily_temperatures_pohang.csv",stringsAsFactors=FALSE)
+  data_t_s<-read.csv(file="daily_temperatures_seoul.csv",stringsAsFactors=FALSE)
+  
+  data_p_n<-read.csv(file="daily_precipitation_national.csv",stringsAsFactors=FALSE)
+  data_p_p<-read.csv(file="daily_precipitation_pohang.csv",stringsAsFactors=FALSE)
+  data_p_s<-read.csv(file="daily_precipitation_seoul.csv",stringsAsFactors=FALSE)
+  
+  data_t<-rbind(data_t_n, data_t_p, data_t_s)
+  data_p<-rbind(data_p_n, data_p_p, data_p_s)
+  data_p[is.na(data_p)] <- 0
+  
+  data <- merge(data_t, data_p, by=c("날짜","지점"))
+  data["날짜"]<-sapply(data["날짜"], function(x) strptime(x,format="%Y-%m-%d"))
   return(data)
 }
 
-data_t_n<-fac2chr(read.csv(file="daily_temperatures_national.csv"))
-data_t_p<-fac2chr(read.csv(file="daily_temperatures_pohang.csv"))
-data_t_s<-fac2chr(read.csv(file="daily_temperatures_seoul.csv"))
 
-data_p_n<-fac2chr(read.csv(file="daily_precipitation_national.csv"))
-data_p_p<-fac2chr(read.csv(file="daily_precipitation_pohang.csv"))
-data_p_s<-fac2chr(read.csv(file="daily_precipitation_seoul.csv"))
 
-data_t<-rbind(data_t_n, data_t_p, data_t_s)
-data_p<-rbind(data_p_n, data_p_p, data_p_s)
-data_p[is.na(data_p)] <- 0
-
-data=merge(data_t, data_p, by=c("날짜","지점"))
-
-data["날짜"]<-sapply(data["날짜"], as.Date)
-      
+weather <- importWeather()
