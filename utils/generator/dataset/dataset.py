46c47fb96b499623bc296a858ba2d35061f10707
beta-lux <beta.underalpha@gmail.com>
2020-07-20 16:01:51 +0900

    make january dataset

--- utils/generator/dataset/dataset.py
+++ utils/generator/dataset/dataset.py
@@ -4,9 +4,6 @@ import sys
 import glob
 import os
 
-problem = pd.read_csv("src/SolarPV_Elec_Problem.csv", index_col=0, parse_dates=True)
-weather = pd.read_csv("src/AVG_weather_15min.csv", index_col=1, parse_dates=True)
-
 
 def get_average_table(weather_frame: pd.DataFrame, period_min: int):
     # 풍향(deg),풍속(m/s),현지기압(hPa),해면기압(hPa),습도(%),일사(MJ/m^2),일조(Sec)
@@ -49,12 +46,9 @@ def concat_dataset(a,b):
     return pd.concat([a, b], axis=1)
 
 
-# make_avg_file()
-processed_file_list = glob.glob("out/weather_avg_15min/diff*.csv")
-w = concat_csv_vertical(processed_file_list, 0)
-size = len(w.index)
-print(size)
-# df_to_csv(concat_csv_vertical(processed_file_list, 0), "dataset1.csv")
-# print(concat_csv_vertical(processed_file_list, 0))
-# print(pd.concat([w, problem], axis=1).head(5))
-pd.concat([w, problem], axis=1).to_csv("dataset_dff.csv")
+def cut_df(_df, start, end):
+    return _df.loc[start:end]
+
+
+df = pd.read_csv("dataset_diff.csv", index_col=0, parse_dates=True)
+df_to_csv(cut_df(df, "2020-01-01", "2020-01-31"), "dataset_diff_JAN.csv")
