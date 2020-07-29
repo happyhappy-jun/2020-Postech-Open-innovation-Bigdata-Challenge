b6da04b34085784f8788c078ee2dceccefe85f98
unajun <bjyoon513@gmail.com>
2020-07-29 15:58:31 +0900

    add hyper parameter tuner

--- learning/LSTM/LSTM_final.py
+++ learning/LSTM/LSTM_final.py
@@ -111,75 +111,92 @@ callback_tensorboard = TensorBoard(log_dir='../23_logs/', histogram_freq=0, writ
 callback_reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, min_lr=1e-7, patience=10,verbose=1)
 callbacks = [callback_early_stopping, callback_checkpoint, callback_tensorboard, callback_reduce_lr]
 
+from tensorflow.keras import layers
+from kerastuner.tuners import RandomSearch
 
 
+def build_model(hp):
+    multi_step_model = tf.keras.models.Sequential()
+
+    multi_step_model.add(tf.keras.layers.GRU(units=hp.Int('units', min_value = 100, max_value=400, step=50), activation=hp.Choice('activation', ["relu", "tanh"]), return_sequences=True, input_shape=(train_X.shape[1], train_X.shape[2])))
+    multi_step_model.add(tf.keras.layers.GRU(units=hp.Int('units', min_value=100, max_value=400, step=50),
+                                             activation=hp.Choice('activation', ["relu", "tanh"]),
+                                             return_sequences=True, input_shape=(train_X.shape[1], train_X.shape[2])))
+
+    #multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu", return_sequences=True, input_shape=(train_X.shape[1], train_X.shape[2])))
+    #multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu",  return_sequences=True))
+    #multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu",  return_sequences=True))
+    #multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu",  return_sequences=True))
+    multi_step_model.add(tf.keras.layers.Dense(1))
+    multi_step_model.compile(optimizer=tf.keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])), loss='mse')
+    return multi_step_model
 
 
-multi_step_model = tf.keras.models.Sequential()
+tuner = RandomSearch(
+    build_model,
+    objective='val_loss',
+    max_trials=5,
+    executions_per_trial=1,
+    directory='H-tuning')
 
-multi_step_model.add(tf.keras.layers.GRU(300, activation = "relu", return_sequences=True, input_shape=(train_X.shape[1], train_X.shape[2])))
-multi_step_model.add(tf.keras.layers.GRU(300, activation = "relu",  return_sequences=True))
-
-#multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu", return_sequences=True, input_shape=(train_X.shape[1], train_X.shape[2])))
-#multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu",  return_sequences=True))
-#multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu",  return_sequences=True))
-#multi_step_model.add(tf.keras.layers.GRU(32, activation = "relu",  return_sequences=True))
-multi_step_model.add(tf.keras.layers.Dense(1))
-
-
-
-
-
-print(f"[+] Available GPUs")
-print(get_available_gpus())
-
-if get_gpu_num() < 2:
-    print(f"[+] Available multiple GPU not found... Just use CPU! XD")
-else:
-    print(f"[+] {get_gpu_num()} GPUs found! Setting to GPU model...")
-    multi_step_model = multi_gpu_model(multi_step_model, gpus=get_gpu_num())
-
-multi_step_model.compile(optimizer=tf.keras.optimizers.Adam(), loss='mse')
-
-history = multi_step_model.fit(train_X, train_y, epochs=EPOCH, batch_size=BATCH_SIZE, validation_data=(test_X, test_y),
+print(tuner.search_space_summary())
+tuner.search(train_X, train_y,epochs=EPOCH, batch_size=BATCH_SIZE, validation_data=(test_X, test_y),
                                verbose=2, shuffle=True, callbacks=callbacks)
-
-try:
-    multi_step_model.load_weights(path_checkpoint)
-except Exception as error:
-    print("Error trying to load checkpoint.")
-
-from numpy import concatenate
-from math import sqrt
-from sklearn.metrics import mean_squared_error
+print(tuner.results_summary())
 
 
-def make_prediction(model, X, y, plot_name):
-    yhat = model.predict(X)[:, :, 0]
-    X_revert = X.reshape((X.shape[0], X.shape[2]))
-    inv_yhat = concatenate((yhat, X_revert), axis = 1)
-    inv_xyhat = scaler.inverse_transform(inv_yhat)
-    inv_yhat = inv_xyhat[:, 0]
-    np.savetxt(plot_name+".csv", inv_yhat, delimiter= ",")
-    y_revert = y.reshape((len(y), 1))
-    inv_y1 = concatenate((y_revert, X_revert), axis = 1)
-    print("after concate: {}".format(inv_y1.shape))
-    inv_y1 = scaler.inverse_transform(inv_y1)
-    inv_y = inv_y1[:, 0]
-    rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-    print('Test RMSE: %.6f' % rmse)
-    print('Test MAE: %.6f' % mean_squared_error(inv_y, inv_yhat))
 
 
-    fig = plt.figure(figsize=(20,6))
-    plt.plot(inv_y, 'b', label = 'true')
-    plt.plot(inv_yhat, 'g', label = 'pred')
-    plt.legend()
-    plt.savefig(plot_name+".png")
+# print(f"[+] Available GPUs")
+# print(get_available_gpus())
+#
+# if get_gpu_num() < 2:
+#     print(f"[+] Available multiple GPU not found... Just use CPU! XD")
+# else:
+#     print(f"[+] {get_gpu_num()} GPUs found! Setting to GPU model...")
+#     multi_step_model = multi_gpu_model(multi_step_model, gpus=get_gpu_num())
+#
+# multi_step_model.compile(optimizer=tf.keras.optimizers.Adam(), loss='mse')
 
-make_prediction(multi_step_model, final_test_X_1, final_test_y_1, "final_test_1")
-make_prediction(multi_step_model, final_test_X_3, final_test_y_3, "final_test_3")
-
-make_prediction(multi_step_model, final_test_X_5, final_test_y_5, "final_test_5")
-make_prediction(multi_step_model, final_test_X, final_test_y, "final_test")
-print(multi_step_model.summary())
+# history = multi_step_model.fit(train_X, train_y, epochs=EPOCH, batch_size=BATCH_SIZE, validation_data=(test_X, test_y),
+#                                verbose=2, shuffle=True, callbacks=callbacks)
+#
+# try:
+#     multi_step_model.load_weights(path_checkpoint)
+# except Exception as error:
+#     print("Error trying to load checkpoint.")
+#
+# from numpy import concatenate
+# from math import sqrt
+# from sklearn.metrics import mean_squared_error
+#
+#
+# def make_prediction(model, X, y, plot_name):
+#     yhat = model.predict(X)[:, :, 0]
+#     X_revert = X.reshape((X.shape[0], X.shape[2]))
+#     inv_yhat = concatenate((yhat, X_revert), axis = 1)
+#     inv_xyhat = scaler.inverse_transform(inv_yhat)
+#     inv_yhat = inv_xyhat[:, 0]
+#     np.savetxt(plot_name+".csv", inv_yhat, delimiter= ",")
+#     y_revert = y.reshape((len(y), 1))
+#     inv_y1 = concatenate((y_revert, X_revert), axis = 1)
+#     print("after concate: {}".format(inv_y1.shape))
+#     inv_y1 = scaler.inverse_transform(inv_y1)
+#     inv_y = inv_y1[:, 0]
+#     rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+#     print('Test RMSE: %.6f' % rmse)
+#     print('Test MAE: %.6f' % mean_squared_error(inv_y, inv_yhat))
+#
+#
+#     fig = plt.figure(figsize=(20,6))
+#     plt.plot(inv_y, 'b', label = 'true')
+#     plt.plot(inv_yhat, 'g', label = 'pred')
+#     plt.legend()
+#     plt.savefig(plot_name+".png")
+#
+# make_prediction(multi_step_model, final_test_X_1, final_test_y_1, "final_test_1")
+# make_prediction(multi_step_model, final_test_X_3, final_test_y_3, "final_test_3")
+#
+# make_prediction(multi_step_model, final_test_X_5, final_test_y_5, "final_test_5")
+# make_prediction(multi_step_model, final_test_X, final_test_y, "final_test")
+# print(multi_step_model.summary())
